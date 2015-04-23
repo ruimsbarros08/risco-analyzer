5b1dca079afe255e7676bec431ecfa3542a1236c
Rui Barros <ruimsbarros08@gmail.com>
2015-04-23 11:00:23 +0100

    vulnerability query bug fixed2

--- scenario_risk.py
+++ scenario_risk.py
@@ -81,9 +81,9 @@ def create_ini_file(job_id, con, folder):
     data = cur.fetchone()
 
     cur.execute("select eng_models_vulnerability_model.type \
-                from jobs_scenario_risk_vulnerability_models, eng_models_vulnerability_model \
-                where jobs_scenario_risk_vulnerability_models.scenario_risk_id = %s \
-                and jobs_scenario_risk_vulnerability_models.vulnerability_model_id = eng_models_vulnerability_model.id", (job_id,))
+                from jobs_scenario_risk_vulnerability_model, eng_models_vulnerability_model \
+                where jobs_scenario_risk_vulnerability_model.scenario_risk_id = %s \
+                and jobs_scenario_risk_vulnerability_model.vulnerability_model_id = eng_models_vulnerability_model.id", (job_id,))
     vulnerability = cur.fetchall()
 
     params = dict(name= data[0],
