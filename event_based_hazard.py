aef9b71caa31635bef36aa67a6847246bce56dec
Rui Barros <ruimsbarros08@gmail.com>
2015-08-06 02:00:42 +0100

    exposure on hazard

--- event_based_hazard.py
+++ event_based_hazard.py
@@ -6,6 +6,7 @@ import json
 import subprocess
 
 from scenario_hazard import create_site_model
+from scenario_damage import create_exposure_model
 from psha_hazard import create_logic_tree_sm, create_logic_tree_gmpe, create_source_model, run, save
 
 PATH = os.path.dirname(os.path.realpath(__file__)) + '/files/'
@@ -46,10 +47,11 @@ def save_ses_ruptures(job_id, hazard_output_id, con):
 		# 			AND a.rupture_id = b.id", (job_id, id[0],))
 		# con.commit()
 
-		cur.execute("INSERT INTO jobs_event_based_hazard_ses_rupture (job_id, output_id, ses_id, rupture_id, rake, magnitude, location, depth, weight) \
-					SELECT %s, foreign_ses_collection.output_id, foreign_ses_rupture.ses_id, foreign_ses_rupture.rupture_id, foreign_probabilistic_rupture.rake, \
+		cur.execute("INSERT INTO jobs_event_based_hazard_ses_rupture (job_id, output_id, ses_id, rupture_id, rake, magnitude, location, depth, weight, rupture_oq) \
+					SELECT %s, foreign_ses_collection.output_id, foreign_ses_rupture.ses_id, foreign_ses_rupture.id, foreign_probabilistic_rupture.rake, \
 					foreign_probabilistic_rupture.magnitude, \
-					ST_GeomFromText('POINT(' || foreign_probabilistic_rupture._hypocenter[1] || ' ' || foreign_probabilistic_rupture._hypocenter[2] || ')', 4326), foreign_probabilistic_rupture._hypocenter[3]  \
+					ST_GeomFromText('POINT(' || foreign_probabilistic_rupture._hypocenter[1] || ' ' || foreign_probabilistic_rupture._hypocenter[2] || ')', 4326), \
+					foreign_probabilistic_rupture._hypocenter[3], foreign_lt_realization.weight, foreign_ses_rupture.rupture_id  \
 					FROM foreign_ses_rupture, foreign_probabilistic_rupture, foreign_ses_collection, \
 					foreign_assoc_lt_rlz_trt_model, foreign_gmf, foreign_lt_realization \
 					WHERE foreign_ses_collection.output_id = %s \
@@ -68,21 +70,32 @@ def start(id, connection):
 	print "Starting calculating Event Based Hazard PSHA hazard: "+str(id)
 
 	cur = connection.cursor()
+
+	cur.execute('select current_database()')
+	db_name = cur.fetchone()[0]
+
+	FOLDER = PATH + db_name + "/event_based_hazard/"+str(id)
+
+	try:
+		os.makedirs(FOLDER)
+	except:
+		pass
+
 	cur.execute('SELECT jobs_classical_psha_hazard.name, st_astext(jobs_classical_psha_hazard.region), jobs_classical_psha_hazard.grid_spacing, \
 				jobs_classical_psha_hazard.sites_type, jobs_classical_psha_hazard.vs30, jobs_classical_psha_hazard.vs30type, jobs_classical_psha_hazard.z1pt0, jobs_classical_psha_hazard.z2pt5, jobs_classical_psha_hazard.site_model_id, \
 				jobs_classical_psha_hazard.random_seed, jobs_classical_psha_hazard.rupture_mesh_spacing, jobs_classical_psha_hazard.truncation_level, jobs_classical_psha_hazard.max_distance, \
 				jobs_classical_psha_hazard.n_lt_samples, jobs_classical_psha_hazard.width_of_mfd_bin, jobs_classical_psha_hazard.area_source_discretization, jobs_classical_psha_hazard.investigation_time, \
 				jobs_classical_psha_hazard.imt_l, jobs_classical_psha_hazard.poes, jobs_classical_psha_hazard.quantile_hazard_curves, jobs_classical_psha_hazard.gmpe_logic_tree_id, jobs_classical_psha_hazard.sm_logic_tree_id, \
-				jobs_event_based_hazard.ses_per_logic_tree_path \
+				jobs_event_based_hazard.ses_per_logic_tree_path, jobs_classical_psha_hazard.locations_type, \
+				st_astext(jobs_classical_psha_hazard.locations), jobs_classical_psha_hazard.exposure_model_id \
 				FROM jobs_classical_psha_hazard, jobs_event_based_hazard \
 				WHERE jobs_event_based_hazard.classical_psha_hazard_ptr_id = %s \
 				AND jobs_event_based_hazard.classical_psha_hazard_ptr_id = jobs_classical_psha_hazard.id', (id,))
 	data = cur.fetchone()
 
-
 	params = dict(type='event_based',
 				name = data[0],
-				region = data[1].split('(')[2].split(')')[0],
+				region = data[1],
 				grid_spacing = data[2],
 				sites_type = data[3],
 				vs30 = data[4],
@@ -103,18 +116,21 @@ def start(id, connection):
 				quantile_hazard_curves = data[19],
 				gmpe_logic_tree_id = data[20],
 				sm_logic_tree_id = data[21],
-				ses_per_logic_tree_path= data[22])
-	
-	cur = connection.cursor()
-	cur.execute('select current_database()')
-	db_name = cur.fetchone()[0]
+				ses_per_logic_tree_path= data[22],
+				locations_type= data[23],
+				locations= data[24],
+				exposure_model= data[25])
 
-	FOLDER = PATH + db_name + "/event_based_hazard/"+str(id)
+	if params['locations_type'] == 'EXPOSURE':
+		create_exposure_model(params['exposure_model'], connection, FOLDER, params['region'])
+		params['region'] = params['region'].split('(')[2].split(')')[0]
 
-	try:
-		os.makedirs(FOLDER)
-	except:
-		pass
+	elif params['locations_type'] == 'GRID':
+		params['region'] = params['region'].split('(')[2].split(')')[0]
+
+	else:
+		params['locations'] = params['locations'].replace('MULTIPOINT (', '').replace(')', '').split(',')
+
 
 	if params['sites_type'] == 'VARIABLE_CONDITIONS':
 		create_site_model(params['site_model_id'], connection, FOLDER)
